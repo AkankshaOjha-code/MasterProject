18749a55dc0bc20f27c14c5daecb00ec0a65c168
hp <hp@DESKTOP-O6NOC91>
2020-09-04 17:13:15 +0530

    test

--- GitLearning/src/main/java/testingGit.java
+++ GitLearning/src/main/java/testingGit.java
@@ -1,3 +1,34 @@
+import java.lang.*;
 class abc{
 	String xyz=null;
+	private String a1 =null;
+	private int a2 =0;
+	public abc(String xyz) {
+		super();
+		this.xyz = xyz;
+	}
+
+	public String getXyz() {
+		return xyz;
+	}
+
+	public void setXyz(String xyz) {
+		this.xyz = xyz;
+	}
+	public String getA1() {
+		return a1;
+	}
+
+	public void setA1(String a1) {
+		this.a1 = a1;
+	}
+
+	public int getA2() {
+		return a2;
+	}
+
+	public void setA2(int a2) {
+		this.a2 = a2;
+	}
+	
 }
